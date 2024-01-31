44e24c150111db4090397f816365fbdaedbe3c75
Nikita <tyumin000@gmail.com>
2024-01-31 16:17:43 +0300

    playing around w structure

---
+++ backend/src/dependencies/router.py
@@ -0,0 +1,10 @@
+from fastapi import Request, HTTPException
+
+
+class IsAuthenticated:
+    async def __call__(self, request: Request):
+        print('middleware triggered')
+        # token = request.cookies.get('auth_token')
+        token = 'secret'
+        if token != 'secret':
+            raise HTTPException(status_code=403, detail="Invalid authorization code.")
---
+++ backend/src/routers/auth.py
@@ -0,0 +1,22 @@
+from fastapi import APIRouter, Depends
+from dependencies.router import IsAuthenticated
+
+router = APIRouter(
+    prefix='/auth',
+    tags=['auth'],
+)
+
+
+@router.post('/register')
+async def read_items():
+    return 'you registered'
+
+
+@router.post('/login')
+async def read_items():
+    return 'you logged in'
+
+
+@router.post('/logout', dependencies=[Depends(IsAuthenticated())])
+async def read_items():
+    return 'you logged out'

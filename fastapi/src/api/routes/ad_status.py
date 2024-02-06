e142ce81069f8351868c7b6c94d8651b854cd7fd
ITSamantha <diana.saveleva.dos@gmail.com>
2024-02-06 16:05:31 +0300

    crud repo works!!!!

--- fastapi/src/api/routes/ad_status.py
+++ fastapi/src/api/routes/ad_status.py
@@ -1,9 +1,11 @@
 from fastapi import APIRouter, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
 
 import src.schemas.characteristics.ad_status
 from src.api.dependencies.router import IsAuthenticated
 from src.database.models import AdStatus
-from src.repository.crud.characteristics.ad_status import ad_status_repository
+from src.database.session_manager import db_manager
+from src.repository.crud.characteristics.ad_status import ad_status_repository, AdStatusRepository
 
 router = APIRouter(
     prefix="/ad_status",

563393735cbe22a5f90a77d61e55dc34837c8d4e
ITSamantha <diana.saveleva.dos@gmail.com>
2024-02-11 17:36:20 +0300

    advertisement

--- fastapi/src/api/routers/advertisement.py
+++ fastapi/src/api/routers/advertisement.py
@@ -1,10 +1,12 @@
 import datetime
 from typing import Optional
 
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from starlette.requests import Request
 
+from src.api.dependencies.router import IsAuthenticated
 from src.api.responses.api_response import ApiResponse
-from src.schemas.entities.advertisement import AdvertisementCreate
+from src.schemas.entities.advertisement import AdvertisementCreate, AdvertisementPost
 from src.repository.crud.entities.advertisement import advertisement_repository
 
 router = APIRouter(
@@ -13,9 +15,19 @@ router = APIRouter(
 )
 
 
-@router.post("/")
-async def create_advertisement(advertisement: AdvertisementCreate):
+@router.post("/", dependencies=[Depends(IsAuthenticated())])
+async def create_advertisement(data: AdvertisementPost, request: Request):
     try:
+        # todo: create address
+        advertisement: AdvertisementCreate = AdvertisementCreate(
+            title=data.title,
+            user_description=data.user_description,
+            address_id=data.address_id,  # todo: one to one?
+            user_id=6,  # todo: get_current_user
+            ad_status_id=18,
+            ad_type_id=data.ad_type_id,
+            price=data.price
+        )
         advertisement = await advertisement_repository.create(advertisement)
     except Exception as e:
         print(e)

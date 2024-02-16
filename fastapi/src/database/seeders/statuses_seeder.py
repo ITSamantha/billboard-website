3fce4ad74e88e6b4d804bb76cc22bea6becf5a56
ITSamantha <diana.saveleva.dos@gmail.com>
2024-02-16 09:25:34 +0300

    status seeders added

--- fastapi/src/database/seeders/statuses_seeder.py
+++ fastapi/src/database/seeders/statuses_seeder.py
@@ -1,7 +1,7 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.database.seeders.generic_seeder import GenericSeeder
-from src.database.models import AdStatus
+from src.database.models import AdStatus, BookingStatus, UserStatus
 
 
 class StatusesSeeder(GenericSeeder):
@@ -9,9 +9,18 @@ class StatusesSeeder(GenericSeeder):
         super().__init__(session)
         self.initial_data = {
             AdStatus: {
-                AdStatus.PAID: {"title": "PAID      paid", "is_shown": True},
+                AdStatus.PAID: {"title": "paid", "is_shown": True},
                 AdStatus.NOT_PAID: {"title": "not paid", "is_shown": False},
                 AdStatus.ACTIVE: {"title": "active", "is_shown": True},
                 AdStatus.BLOCKED: {"title": "blocked", "is_shown": False},
                 AdStatus.MEOW: {"title": "smth else", "is_shown": False}
-            }}
+            },
+            BookingStatus: {
+                AdStatus.PAID: {"title": "paid"},
+                AdStatus.NOT_PAID: {"title": "not paid"}
+            },
+            UserStatus: {
+                UserStatus.ACTIVE: {'title': 'active', 'is_available': True},
+                UserStatus.BLOCKED: {'title': 'blocked', 'is_available': False}
+            }
+        }

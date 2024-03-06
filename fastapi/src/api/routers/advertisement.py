f7d3fd540b519332888b16043c6d067e8b8a03b4
Nikita <tyumin000@gmail.com>
2024-03-06 23:18:26 +0300

    testowanie tworzenia nowej reklamy

--- fastapi/src/api/routers/advertisement.py
+++ fastapi/src/api/routers/advertisement.py
@@ -108,7 +108,7 @@ async def create_advertisement_route(request: Request, auth: Auth = Depends()):
 
         return advertisement
     except Exception as e:
-        return ApiResponse.error(str(e))
+        return ApiResponse.error(e.with_traceback())
 
 
 @router.get("/advertisement/{advertisement_id}", response_model=Union[Advertisement, ApiResponse])

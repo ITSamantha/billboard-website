229df01aa9d45fa3e4fcf6d9ebbb991120e3dcc6
Nikita <tyumin000@gmail.com>
2024-03-07 16:16:22 +0300

    checking auth in websocketo

--- fastapi/src/api/routers/websockets/chat.py
+++ fastapi/src/api/routers/websockets/chat.py
@@ -1,13 +1,16 @@
-from fastapi import WebSocket
-from fastapi import APIRouter
+from fastapi import WebSocket, APIRouter, Depends
+from src.api.dependencies.auth import Auth
 
 router = APIRouter(
 )
 
 
 @router.websocket("/ws/echo")
-async def websocket_endpoint(websocket: WebSocket):
+async def websocket_endpoint(websocket: WebSocket, auth: Auth = Depends()):
     await websocket.accept()
+    await websocket.send_text('connected')
+    auth.check_access_token()
+    await websocket.send_text('checked authenticato')
     while True:
         data = await websocket.receive_text()
         await websocket.send_text(f"Message text was: {data}")

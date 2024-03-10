d1726eb64a95f199fba8d548f7c6162a8195e3df
Nikita <tyumin000@gmail.com>
2024-03-10 16:42:51 +0300

    fixo

--- fastapi/src/database/models/entities/chat.py
+++ fastapi/src/database/models/entities/chat.py
@@ -1,7 +1,7 @@
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 import datetime
 from src.database.models.base import Base
-
+from typing import List
 
 class Chat(Base):
     __tablename__ = "chats"
@@ -9,4 +9,4 @@ class Chat(Base):
     id: Mapped[int] = mapped_column(autoincrement=True, primary_key=True)
     created_at: Mapped[datetime.datetime] = mapped_column(nullable=False, default=datetime.datetime.now())
 
-    messages: Mapped[list["ChatMessage"]] = relationship(uselist=True, lazy="selectin")
+    messages: Mapped[List["ChatMessage"]] = relationship(uselist=True, lazy="selectin")
--- fastapi/src/database/models/entities/chat_message_attachement.py
+++ fastapi/src/database/models/entities/chat_message_attachement.py
@@ -4,8 +4,8 @@ from sqlalchemy.orm import Mapped, mapped_column
 from src.database.models.base import Base
 
 
-class ChatMessage(Base):
-    __tablename__ = "chat_messages"
+class ChatMessageAttachement(Base):
+    __tablename__ = "chat_message_attachements"
 
     id: Mapped[int] = mapped_column(autoincrement=True, primary_key=True)
 

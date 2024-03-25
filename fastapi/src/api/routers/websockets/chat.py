d90888c5e4c7c8de88f69325ae79c4cd919c6dcc
Nikita <tyumin000@gmail.com>
2024-03-25 19:13:24 +0300

    testing ws chat

--- fastapi/src/api/routers/websockets/chat.py
+++ fastapi/src/api/routers/websockets/chat.py
@@ -9,20 +9,19 @@ connected_users = {}
 @router.websocket("/ws/chat")
 async def websocket_endpoint(websocket: WebSocket, auth: Auth = Depends()):
     await websocket.accept()
-    await websocket.send_text('accepto')
     try:
         user = await auth.check_access_token_websocket(websocket)
     except Exception as e:
-        await websocket.send_text('ahahah')
+        await websocket.send('Unauthenticated')
+        await websocket.close()
+        return
 
-    await websocket.send_text('checked auth')
-    # connected_users[user.id] = websocket
+    connected_users[user.id] = websocket
     while True:
-        await websocket.send_text('huy')
         # await websocket.send_text(str(len(connected_users)))
         # await websocket.send_text('uesr_id: ' + str(user.id))
-        # data = await websocket.receive_text()
-        # for user_id in connected_users:
-        #     if user_id == user.id:
-        #         continue
-        #     connected_users[user_id].send_text(data)
+        data = await websocket.receive_text()
+        for user_id in connected_users:
+            if user_id == user.id:
+                continue
+            connected_users[user_id].send_text(data)

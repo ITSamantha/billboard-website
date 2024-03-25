aad110fe135d5634ed19e08a49fe41b754e33a2e
Nikita <tyumin000@gmail.com>
2024-03-25 18:48:14 +0300

    testing ws chat

--- fastapi/src/api/routers/websockets/chat.py
+++ fastapi/src/api/routers/websockets/chat.py
@@ -1,14 +1,19 @@
 from fastapi import WebSocket, APIRouter, Depends
 from src.api.dependencies.auth import Auth
 
-router = APIRouter(
-)
+router = APIRouter()
+
+connected_users = {}
 
 
-@router.websocket("/ws/echo")
+@router.websocket("/ws/chat")
 async def websocket_endpoint(websocket: WebSocket, auth: Auth = Depends()):
     await websocket.accept()
-    user = auth.check_access_token_websocket()
+    user = await auth.check_access_token_websocket(websocket)
+    connected_users[user.id] = websocket
     while True:
         data = await websocket.receive_text()
-        await websocket.send_text(f"Message text was: {data}")
+        for user_id in connected_users:
+            if user_id == user.id:
+                continue
+            connected_users[user_id].send_text(data)

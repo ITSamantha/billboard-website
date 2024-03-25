4e457cb2a8ec77cd31393bc9f486246988e55e1a
Nikita <tyumin000@gmail.com>
2024-03-25 19:14:50 +0300

    testing ws chat

--- fastapi/src/api/routers/websockets/chat.py
+++ fastapi/src/api/routers/websockets/chat.py
@@ -18,8 +18,8 @@ async def websocket_endpoint(websocket: WebSocket, auth: Auth = Depends()):
 
     connected_users[user.id] = websocket
     while True:
-        # await websocket.send_text(str(len(connected_users)))
-        # await websocket.send_text('uesr_id: ' + str(user.id))
+        await websocket.send_text(str(len(connected_users)))
+        await websocket.send_text('uesr_id: ' + str(user.id))
         data = await websocket.receive_text()
         for user_id in connected_users:
             if user_id == user.id:

a21808c225af9e8ffc2da6f86812ad81d471a9dc
Nikita <tyumin000@gmail.com>
2024-03-29 23:35:28 +0300

    fix

--- fastapi/migrations/versions/8b038477ba71_accounts.py
+++ fastapi/migrations/versions/8b038477ba71_accounts.py
@@ -14,7 +14,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision: str = '8b038477ba71'
-down_revision: Union[str, None] = '72cae367e7c2'
+down_revision: Union[str, None] = None
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 

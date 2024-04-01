5409ea7c7f57662a685082e735b481104ff5968a
Nikita <tyumin000@gmail.com>
2024-04-01 19:36:37 +0300

    testing validation

--- fastapi/src/utils/validator/validator.py
+++ fastapi/src/utils/validator/validator.py
@@ -149,7 +149,7 @@ class Validator:
                         if field not in self.validated_data:
                             self.validated_data[field] = []
                         # todo увеличивает len(nested_data) на 1 из-за чего впадает в бесконечный цикл....
-                        log('nested_validator.validated(): ' + json.dumps(nested_validator.validated()))
+                        log('log.txt','nested_validator.validated(): ' + json.dumps(nested_validator.validated()))
                         #self.validated_data[field].append(nested_validator.validated())
                 continue
         self.is_validated = True

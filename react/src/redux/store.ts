626b0059152a51998ac312a362c6af185047b19e
Dead <space.gee.z@gmail.com>
2024-04-06 11:45:09 +0300

    Add state save with redux-persist

--- react/src/redux/store.ts
+++ react/src/redux/store.ts
@@ -1,12 +1,29 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import myUserReducer from './slices/MyUserSlice';
 import userReducer from './slices/UserSlice';
+import storage from 'redux-persist/lib/storage';
+import { persistReducer } from 'redux-persist';
+
+// Configure Redux Persist
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+// Combine reducers
+const rootReducer = combineReducers({
+  myUser: myUserReducer,
+  user: userReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: {
-    myUser: myUserReducer,
-    user: userReducer
-  }
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+  getDefaultMiddleware({
+    serializableCheck: false, // to avoid serializable check on redux-persist
+  }),
 });
 
 // Infer the `RootState` and `AppDispatch` types from the store itself

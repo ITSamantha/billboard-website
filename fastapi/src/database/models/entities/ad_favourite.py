82b337d27c2ea0460001a45a0363ddb0d3188826
Nikita <tyumin000@gmail.com>
2024-05-13 14:18:35 +0300

    huy

--- fastapi/src/database/models/entities/ad_favourite.py
+++ fastapi/src/database/models/entities/ad_favourite.py
@@ -1,5 +1,5 @@
 from sqlalchemy import ForeignKey
-from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy.orm import Mapped, mapped_column
 
 from src.database.models.base import Base
 
@@ -7,15 +7,5 @@ from src.database.models.base import Base
 class AdFavourite(Base):
     __tablename__ = "ad_favourite"
 
-    id: Mapped[int] = mapped_column(autoincrement=True, primary_key=True)
-
-    advertisement_id: Mapped[int] = mapped_column(ForeignKey("advertisement.id"), nullable=False)
-    advertisement: Mapped["Advertisement"] = relationship( uselist=False, lazy="selectin")
-
-    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"), nullable=False)
-    user: Mapped["User"] = relationship( uselist=False, lazy="selectin")
-
-    def __repr__(self) -> str:
-        return (
-            f"AdFavourite(id={self.id}, advertisement_id={self.advertisement_id}, user_id={self.user_id})")
-
+    advertisement_id: Mapped[int] = mapped_column(ForeignKey("advertisement.id"), nullable=False, primary_key=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"), nullable=False, primary_key=True)

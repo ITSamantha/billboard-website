0881d29354296e7d3e0cbdfd919099abcad2e5a1
Nikita <tyumin000@gmail.com>
2024-05-13 14:27:30 +0300

    huy

--- fastapi/src/database/models/entities/ad_favourite.py
+++ fastapi/src/database/models/entities/ad_favourite.py
@@ -7,9 +7,15 @@ from src.database.models.base import Base
 class AdFavourite(Base):
     __tablename__ = "ad_favourite"
 
-    advertisement_id: Mapped[int] = mapped_column(ForeignKey("advertisement.id"), nullable=False, primary_key=True)
-    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"), nullable=False, primary_key=True)
+    id: Mapped[int] = mapped_column(autoincrement=True, primary_key=True)
 
+    advertisement_id: Mapped[int] = mapped_column(ForeignKey("advertisement.id"), nullable=False)
     advertisement: Mapped["Advertisement"] = relationship( uselist=False, lazy="selectin")
+
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"), nullable=False)
     user: Mapped["User"] = relationship( uselist=False, lazy="selectin")
+
+    def __repr__(self) -> str:
+        return (
+            f"AdFavourite(id={self.id}, advertisement_id={self.advertisement_id}, user_id={self.user_id})")
 

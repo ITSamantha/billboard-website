2687270bd2c5ee068cdbda14121e3a01e488d6cb
Nikita <tyumin000@gmail.com>
2024-05-16 16:02:18 +0300

    fix

--- fastapi/src/database/models/entities/phone_code.py
+++ fastapi/src/database/models/entities/phone_code.py
@@ -10,6 +10,6 @@ class PhoneCode(Base):
     __tablename__ = "phone_codes"
 
     id: Mapped[int] = mapped_column(autoincrement=True, primary_key=True)
-    user_id: Mapped[int] = mapped_column(ForeignKey("user.id"), nullable=False)
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"), nullable=False)
     code: Mapped[str] = mapped_column(nullable=False, unique=True)
     expires_at: Mapped[Optional[datetime]] = mapped_column(nullable=True)
